c0573fd4e4acde201183dc2e78d92bc2b5df7e3b
HAMIT KARAHAN <hamit.karahan@ext.digiturk.com.tr>
2022-05-09 15:24:20 +0300

    UI layer configured

---
+++ src/Example.TodoWebApp.UI/Controllers/TodoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.TodoWebApp.UI.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult List()
+        {
+
+            return View();
+        }
+    }
+}
--- src/Example.TodoWebApp.UI/Program.cs
+++ src/Example.TodoWebApp.UI/Program.cs
@@ -1,11 +1,22 @@
 using Example.TodoWebApp.Bussiness.DependencyResolvers.Microsoft;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
 builder.Services.AddBussinessModule();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+    RequestPath = "/node_modules"
+});
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Todo}/{action=List}/{id?}");
 
 app.Run();
